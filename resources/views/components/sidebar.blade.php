59c53d6aebdc2528020be530a95d36bc91b60008
YusufHusain243 <husainyusuf382@gmail.com>
2022-08-25 18:21:46 +0700

    add CRUD user

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -9,13 +9,13 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
                 <li class="nav-item">
-                    <a href="/" class="nav-link active">
+                    <a href="/" class="nav-link  {{ $title === 'dashboard' ? 'active' : '' }}">
                         <i class="fas fa-circle nav-icon"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="/user" class="nav-link">
+                    <a href="/user" class="nav-link  {{ $title === 'user' ? 'active' : '' }}">
                         <i class="fas fa-circle nav-icon"></i>
                         <p>User</p>
                     </a>
