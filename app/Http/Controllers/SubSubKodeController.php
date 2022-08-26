16cf50cd6c9a682f06085a067a377082e49b76ec
YusufHusain243 <husainyusuf382@gmail.com>
2022-08-26 22:05:26 +0700

    add Crud Sub Sub Kode

---
+++ app/Http/Controllers/SubSubKodeController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sub_SubKode;
+use App\Models\SubKode;
+use Illuminate\Http\Request;
+
+class SubSubKodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sub_sub_kodes = Sub_SubKode::all();
+        $sub_kodes = SubKode::all();
+        return view('pages/sub_sub_kode', [
+            "title" => "sub_sub_kode",
+            "sub_sub_kodes" => $sub_sub_kodes,
+            "sub_kodes" => $sub_kodes,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -33,7 +33,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="/sub sub-kode" class="nav-link">
+                    <a href="/sub-sub-kode" class="nav-link">
                         <i class="fas fa-circle nav-icon"></i>
                         <p>Daftar Sub Sub-Kode</p>
                     </a>
