a456c2dbf113457c92938eb2ae0f96db05991cb0
albretost09 <107251731+albretost09@users.noreply.github.com>
2022-08-26 13:59:46 +0700

    Revert "Dev"

--- resources/views/components/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2022 | </strong>Sistem Akuntansi Gereja Sinar Kasih
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Created By</b> @Noverina Eka Shinta
-    </div>
-</footer>
